bf422186bb5e4c16fc4457dc2a3aec01f0d9d691
oberdev <qovalski@gmail.com>
2018-09-16 21:38:02 +0300

    lol

--- app/src/Contacts/contacts.js
+++ app/src/Contacts/contacts.js
@@ -1,8 +1,10 @@
 import IMask from 'imask';
 
 export default $(function() {
+	if (document.getElementById('phone') != null){
 	var phoneMask = new IMask(
   document.getElementById('phone'), {
     mask: '+0(000)000-00-00'
   });
+}
 });
--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var gulp           = require('gulp'),
 		browserSync({
 			server: {
 				baseDir: 'app',
-				index: "cart.html"
+				index: "index.html"
 			},
 			notify: false,
 			// tunnel: true,
