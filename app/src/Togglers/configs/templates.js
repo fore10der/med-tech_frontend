9e0e36aa5ebc2a2aa990f076bc9bac66ec7c6b91
oberdev <qovalski@gmail.com>
2018-09-25 03:18:01 +0300

    lmao

--- app/src/Togglers/configs/templates.js
+++ app/src/Togglers/configs/templates.js
@@ -6,8 +6,11 @@ export default {
 	}, ''),
 	"partners": "../db/devices.json",
 	"team": "../db/devices.json",
-	"offices": (results) => results.reduce(function (html,object) {
+	"offices": (results) => {
+			$("circle").remove();
+			results.reduce(function (html,object) {
 			placePoint(object.region,object.translations.ru.title,object.translations.ru.description)
 			return html + '<div class="col-md-6 team__workers-worker"><div class="row team__workers-wrapper"><div class="team__workers-img-wrapper col-md-4"><div style="background-image: url(' + object.image + ')" class="team__workers-img"></div></div><div class="team__workers-text col-md-8"><h3 class="team__workers-title">' + object.translations.ru.title + '</h3><p class="team__workers-desc offices__office-desc">' + object.translations.ru.description + '</p><ul class="footer__contacts-list offices__contacts-list"><li class="footer__contacts-elem footer__contacts-address offices__contacts-elem"><span>Адрес</span> : ' + object.translations.ru.adress + '</li><li class="footer__contacts-elem footer__contacts-phone offices__contacts-elem"><span>Phone</span> : ' + object.phone + '</li><li class="footer__contacts-elem footer__contacts-email offices__contacts-elem"><span>Email</span> : ' + object.email + '</li></ul></div></div></div>'
 	}, '')
+}
 }
